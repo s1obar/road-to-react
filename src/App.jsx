e464469a600d934d426e498c387c37c6bbd6c78a
Slobodan Barisic <slobodan.barisic@zooplus.com>
2023-03-11 01:14:01 +0100

    return JSX for each item in the list first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import * as React from 'react'
 
+const welcome = {
+    greeting: 'Hey',
+    title: 'React!'
+}
 
+const numbers = [1, 2, 3]
+
+function renderNumbersFromArray(){
+    return numbers.map(num => <h3>{num}</h3>)
+}
+
+function getWelcome(){
+    return welcome;
+}
 
 function App() {
-    const title = 'Hello React!'
     return (
         <div>
-            <h1>{ title }</h1>
+            <h1>
+                { getWelcome().greeting } { getWelcome().title }
+            </h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text"/>
+            { renderNumbersFromArray() }
         </div>
     )
 }
