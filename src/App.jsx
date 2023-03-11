de9382c6729627813099edbac7753ff36c0de5b7
Slobodan Barisic <slobodan.barisic@zooplus.com>
2023-03-11 11:58:36 +0100

    dealing with lists in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import * as React from 'react'
 
-const title = 'React!'
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    },
+    {
+        title: 'Redux',
+        url: 'https://reactjs.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    }
+]
 
 function App() {
     return (
         <div>
-            <h1>
-                Hello World
-            </h1>
+            <h1>Road to React</h1>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text"/>
+            <hr />
+            <ul>
+                { list.map(function (item){
+                    return (
+                        <li key={item.objectID}>
+                            <span>{item.title}</span>
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
