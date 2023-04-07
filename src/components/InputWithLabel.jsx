c21920a8baa2f23b690d69f140a15e82f92be550
Slobodan Barisic <slbarisic@gmail.com>
2023-04-07 19:03:34 +0200

    autofocus - imperative implementation using useRef hook

--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -2,17 +2,21 @@ import * as React from 'react'
 
 const InputWithLabel = ({id, label, type = 'text', value, children, onInputChange, isFocused, autoComplete = 'off'}) => {
     console.log("Search renders")
+    const inputRef = React.useRef(null);
 
-    // const handleBlur = (event) => {
-    //     console.log(event.target.value)
-    // }
-        return (
-            <>
-                <label htmlFor={id}>{children}</label>
-                &nbsp;
-                <input id={id} autoComplete={autoComplete} type={type} value={value} autoFocus={isFocused} onChange={onInputChange} /*onBlur={handleBlur}*//>
-            </>
-        )
+    React.useEffect(() => {
+        if(isFocused && inputRef.current){
+            inputRef.current.focus()
+        }
+    }, [isFocused])
+
+    return (
+        <>
+            <label htmlFor={id}>{children}</label>
+            &nbsp;
+            <input id={id} autoComplete={autoComplete} ref={inputRef} type={type} value={value} onChange={onInputChange}/>
+        </>
+    )
 }
 
 export default InputWithLabel
