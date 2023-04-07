7259a58e2880a54180ce499369f43dee1044a30c
Slobodan Barisic <slobodan.barisic@zooplus.com>
2023-04-07 14:44:27 +0200

    rest and spread operators syntax

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 
-const Item = ({item: {url, title, author, num_comments, points}}) => {
+const Item = ({url, title, author, num_comments, points}) => {
     console.log("Item renders")
 
     return <li>
         <span>{title}</span>
         <span>
-                        <a href={url}>{title}</a>
-                    </span>
+            <a href={url}>{title}</a>
+        </span>
         <span>{author}</span>
         <span>{num_comments}</span>
         <span>{points}</span>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,7 +4,14 @@ import Item from "./Item.jsx";
 const List = ({items}) => {
     console.log("List renders")
 
-    const itemElements = items.map((item) => <Item key={item.objectID} item={item}/>)
+
+    /**
+        Different syntax for spread operator and rest operator
+        const itemElementsSpreadOperator = items.map((item) => <Item key={item.objectID} item={item}/>)
+        const itemElementsRestOperator = items.map(({objectID, ...item}) => <Item key={objectID} {...item}/>)
+    **/
+
+    const itemElements = items.map(({objectID, ...item}) => <Item key={objectID} {...item}/>)
 
     return (
         <ul>
